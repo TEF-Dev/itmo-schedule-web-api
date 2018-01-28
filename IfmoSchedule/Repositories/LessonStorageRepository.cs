2fb6aa7422100fc701d42474126e51f09254e19f
Maksim Barkalov <maks@fizmatspb.ru>
2018-01-28 15:11:52 +0300

    Added getting of shedule

--- IfmoSchedule/Repositories/LessonStorageRepository.cs
+++ IfmoSchedule/Repositories/LessonStorageRepository.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections.Generic;
 using IfmoSchedule.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace IfmoSchedule.Repositories
 {
     public class LessonStorageRepository
     {
         private static List<LessonModel> Data;
-        private static void UpdateFromApi()
+        private static string constructor = "http://mountain.ifmo.ru/api.ifmo.ru/public/v1/schedule_lesson_group/";
+            
+        private static void UpdateFromApi(string groupName)
         {
-            //TODO: Make request, parse json
-        }
+            string address = $"{constructor}{groupName}";
+            HttpClient query = new HttpClient();
+            var result = query.GetAsync(address).Result.Content.ReadAsStringAsync().Result;
+            var lessonsObject = JObject.Parse(result);
+            var lessons = lessonsObject["schedule"];
+            Data = lessons.ToObject<List<LessonModel>>();
 
-        static LessonStorageRepository()
-        {
-            UpdateFromApi();
         }
 
         public LessonStorageRepository()
