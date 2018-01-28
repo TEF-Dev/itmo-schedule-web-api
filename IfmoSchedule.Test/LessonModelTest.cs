851f24e7f6bf362ba3d2358badaf34f6f845fd40
Inredika WhiteBlack <inredikawb@gmail.com>
2018-01-28 15:15:25 +0300

    Add some test

---
+++ IfmoSchedule.Test/LessonModelTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using IfmoSchedule.Repositories;
+using IfmoSchedule.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IfmoSchedule.Test
+{
+    [TestClass]
+    public class LessonModelTest
+    {
+        [TestMethod]
+        public void ModelPropertyTest()
+        {
+            var repository = new LessonStorageRepository();
+            var data = repository.GetAllLesson().FirstOrDefault();
+
+            Assert.IsNotNull(data);
+
+            int? day = data.DayOfWeek;
+            Assert.IsNotNull(day);
+            Assert.IsNotNull(data.Place);
+            Assert.IsNotNull(data.Room);
+            Assert.IsNotNull(data.Status);
+            Assert.IsNotNull(data.Teacher);
+            Assert.IsNotNull(data.TimeBegin);
+            Assert.IsNotNull(data.Title);
+            Assert.IsNotNull(data.WeekType);
+        }
+    }
+}
--- IfmoSchedule/Models/LessonModel.cs
+++ IfmoSchedule/Models/LessonModel.cs
@@ -5,34 +5,34 @@ namespace IfmoSchedule.Models
     public class LessonModel
     {
         [JsonProperty("data_day")]
-        public int DayOfWeek { get; set; }
+        public int? DayOfWeek { get; }
 
         [JsonProperty("status")]
-        public string Status { get; set; }
+        public string Status { get; }
 
         [JsonProperty("data_week")]
-        public int WeekType { get; set; }
+        public int? WeekType { get; }
 
         [JsonProperty("subj_time")]
-        public string FullTime { get; set; }
+        public string FullTime { get; }
 
         [JsonProperty("room")]
-        public string Room { get; set; }
+        public string Room { get; }
 
         //TODO: Custom setter
         [JsonProperty("place")]
-        public string Place { get; set; }
+        public string Place { get; }
 
         [JsonProperty("title")]
-        public string Title { get; set; }
+        public string Title { get; }
 
         [JsonProperty("person")]
-        public string Teacher { get; set; }
+        public string Teacher { get; }
 
         [JsonProperty("start_time")]
-        public string TimeBegin { get; set; }
+        public string TimeBegin { get; }
 
         [JsonProperty("end_time")]
-        public string TimeEnd { get; set; }
+        public string TimeEnd { get; }
     }
 }
